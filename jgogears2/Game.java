c8e0cf4ae18fbfaa1f6f40f3a4f52d5154edecb8
stuart yeates <syeates@gmail.com>
2013-10-29 19:37:31 +1300

    update

--- jgogears2/Game.java
+++ jgogears2/Game.java
@@ -4,9 +4,17 @@ import java.util.Properties;
 import java.util.List;
 
 public interface Game {
-	
+
 	public Properties getProperties();
-	
+
 	public List<Move> getMoves();
+
+	public List<Board> getBoards();
+
+	public boolean isFinished();
+
+	public boolean isScored();
+
+	public boolean GetRuleset();
 
 }
