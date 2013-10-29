a956b1b38f55f5252d3ece99f9d4146ab4d80737
stuart yeates <syeates@gmail.com>
2013-10-29 19:54:50 +1300

    update

--- jgogears2/Board.java
+++ jgogears2/Board.java
@@ -1,39 +1,48 @@
 package jgogears2;
 
 import java.util.List;
+/**
+ * A Board represents a particular state of the board at a particular point in time.
+ * 
+ * It doesn't store complete game state, since some Ko rules traverse all previous board states.
+ * 
+ * @author syeates@gmail.com
+ *
+ */
 
 public interface Board {
 	/**
-	 * 
+	 * What colour is this vertex?
 	 * @param row
 	 * @param column
 	 * @return
 	 */
 		public short getColour(short row, short column);
 		/**
-		 * 
+		 * Return a new Board based on the current board and the given move
 		 * @param move
 		 * @return
 		 */
 		public Board playMove(Move move);
 		/**
-		 * 
+		 * Is the given move legal?
 		 * @param move
 		 * @return
 		 */
 		public boolean isLegal(Move move);
 		/**
+		 * What are all the legal moves?
 		 * 
 		 * @return
 		 */
 		public List<Move> getLegalMoves();
 		/**
-		 * 
+		 * Is black to play next?
 		 * @return
 		 */
 		public boolean blackToPlay();
 		/**
-		 * 
+		 * What game is this board a part of?
 		 * @return
 		 */
 		public Game getGame();
