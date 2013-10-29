a2956c05d47acaa853f4a9cd439f5a60732e9839
stuart yeates <syeates@gmail.com>
2013-10-29 19:51:50 +1300

    update

--- jgogears2/Board.java
+++ jgogears2/Board.java
@@ -10,12 +10,31 @@ public interface Board {
 	 * @return
 	 */
 		public short getColour(short row, short column);
-		
+		/**
+		 * 
+		 * @param move
+		 * @return
+		 */
 		public Board playMove(Move move);
-		
+		/**
+		 * 
+		 * @param move
+		 * @return
+		 */
 		public boolean isLegal(Move move);
-		
+		/**
+		 * 
+		 * @return
+		 */
 		public List<Move> getLegalMoves();
-		
-
+		/**
+		 * 
+		 * @return
+		 */
+		public boolean blackToPlay();
+		/**
+		 * 
+		 * @return
+		 */
+		public Game getGame();
 }
