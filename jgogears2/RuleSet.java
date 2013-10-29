5df0c74f75475c550e1a638f4c062f433086469e
stuart yeates <syeates@gmail.com>
2013-10-30 08:55:33 +1300

    update

--- jgogears2/RuleSet.java
+++ jgogears2/RuleSet.java
@@ -3,10 +3,14 @@ package jgogears2;
 import java.util.Properties;
 
 public interface RuleSet {
+	
 	public Properties getHandicaps();
+	
 	public void placeHandicaps(Board b, short count);
 	
+	public String getName();
 	
+	public boolean isLegalMove(Board board, Move move);
 
 	
 }
