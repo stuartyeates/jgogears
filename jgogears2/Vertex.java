f8ab8add4aa5a7d2e0f17a9646137e15e7befccb
stuart yeates <syeates@gmail.com>
2013-10-30 21:49:58 +1300

    update

--- jgogears2/Vertex.java
+++ jgogears2/Vertex.java
@@ -1,5 +1,16 @@
 package jgogears2;
 
 public class Vertex {
+	public short row;
+	public short column;
+	public Vertex(short row, short column){
+			this.row=row;
+			this.column=column;
+	}
+	public Vertex(int row, int column){
+		this.row=(short)row;
+		this.column=(short)column;
+	
+	}
 
 }
