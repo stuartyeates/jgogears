6aec134f73d500f79fa017bfa93d01ed2c9613ea
stuart yeates <syeates@gmail.com>
2013-11-16 17:03:42 +1300

    update

---
+++ jgogears/BoardTest2.java
@@ -0,0 +1,127 @@
+package jgogears;
+
+import junit.framework.TestCase;
+
+public class BoardTest2 extends TestCase {
+
+	public final void testColourString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNewBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNewBoardBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNewBoardInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNewBoardIntRuleSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testParseColour() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardBoardMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardIntRuleSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardRuleSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardShort() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testBoardShortRuleSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testCopydata() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetAllLegalMoves() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetAllLegalVertexes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetColourIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetColourVertex() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetRuleSet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetZobrist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testInit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsOffBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsWayOffBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNewBoardMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetColour() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetZobrist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ jgogears/Statics.java
@@ -0,0 +1,5 @@
+package jgogears;
+
+public class Statics {
+
+}
