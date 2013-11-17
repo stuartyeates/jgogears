dbaab244dbbddcc38f5ca73307eb696cb9f8df5b
stuart yeates <syeates@gmail.com>
2013-11-18 09:27:26 +1300

    update

--- jgogears/BoardTest2.java
+++ jgogears/BoardTest2.java
@@ -4,51 +4,12 @@ import junit.framework.TestCase;
 
 public class BoardTest2 extends TestCase {
 
-	public final void testColourString() {
-		for (short i = 0; i <= Statics.MAX_BOARD_SIZE; i++) 
-			for (short j = 0; j <= Statics.MAX_BOARD_SIZE; j++) 
-				for (short k = 0; k <= Statics.DEFAULT_BOARD_SIZE; k++) {
-					{
-					Move move = new Move(i, j, Statics.COLOUR_BLACK);
-					Move move2 = new Move(move.toString());
-					assertNotNull(move);
-					assertNotNull(move2);
-					assertTrue(move.equals(move2));
-					}
-					{
-					Move move = new Move(i, j, Statics.COLOUR_WHITE);
-					Move move2 = new Move(move.toString());
-					assertNotNull(move);
-					assertNotNull(move2);
-					assertTrue(move.equals(move2));
-					}
-				}
-		
-
-	}
-
-	public final void testNewBoard() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public final void testNewBoardBoolean() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public final void testNewBoardInt() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public final void testNewBoardIntRuleSet() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	public final void testParseColour() {
-		fail("Not yet implemented"); // TODO
-	}
-
 	public final void testBoard() {
-		fail("Not yet implemented"); // TODO
+		Board board = new Board();
+		Board board2 = new Board();
+		assertTrue(board2.equals(board));
+		assertFalse(board2.equals(this));
+		
 	}
 
 	public final void testBoardBoardMove() {
